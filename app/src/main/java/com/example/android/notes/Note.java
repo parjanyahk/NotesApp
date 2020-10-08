c975f50ce3bef013b497131c43da610362c05a4d
Parjanya HK <parjanyahk123@gmail.com>
2020-10-08 21:41:07 +0530

    note java file 1

--- app/src/main/java/com/example/android/notes/Note.java
+++ app/src/main/java/com/example/android/notes/Note.java
@@ -1,10 +1,41 @@
 package com.example.android.notes;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
 
 @Entity(tableName = "note")
 public class Note implements Serializable {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "title")
+    private String title;
+    @ColumnInfo(name = "note_text")
+    private String noteText;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
 }
